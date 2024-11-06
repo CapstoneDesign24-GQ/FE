ec79739f330089e36cabdadb2802216a7021c407
jiHeeFlee <esder1310@naver.com>
2024-11-06 16:18:57 +0900

    docs : 정적 요소 추가

--- src/assets/constants/Crops.js
+++ src/assets/constants/Crops.js
@@ -1,4 +1,8 @@
-export const Crops={
-    header:'작물 선택',
-    elements:['감귤','고추','콩','딸기','사과']
+import { BsQuestionCircleFill } from 'react-icons/bs';
+
+export const Crops = {
+    header: '작물 선택',
+    icon: <BsQuestionCircleFill style={{ margin: '0px' }} size={22} />,
+    toggleHeader: '작물 선택',
+    elements: ['감귤', '고추', '콩', '딸기', '사과']
 }
--- src/assets/constants/Locations.js
+++ src/assets/constants/Locations.js
@@ -1,4 +1,8 @@
+import { BsGeoFill } from 'react-icons/bs';
+
 export const Locations={
-    header : '위치 입력(도 단위)',
+    header : '지역 선택(도 단위)',
+    icon:<BsGeoFill style={{ margin: '0px' }} size={22}/>,
+    toggleHeader :'지역 선택',
     elements : ['충청도']
 }
