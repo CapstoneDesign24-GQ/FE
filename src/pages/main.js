b12834f4fae79ed1cad7520c4e24087fb08f62f5
jiHeeFlee <esder1310@naver.com>
2024-11-06 15:53:48 +0900

    feat(Main.js) : toggle 안 button 선택시 toggle의 header가 해당 값으로 변하게 구현

--- src/pages/main.js
+++ src/pages/main.js
@@ -136,8 +136,10 @@ const Main = () => {
             value={inputData.location}
             onClick={handleLocationButton}
             onToggle={handleToggle}
-            header={Locations.header}
+            header={inputData.location || Locations.header}
             elements={Locations.elements}
+            icon={Locations.icon}
+            toggleHeader={Locations.toggleHeader}
           />
         </ComponentsContainer>
         <ComponentsContainer>
@@ -145,8 +147,10 @@ const Main = () => {
             value={inputData.crop}
             onClick={handleCropButton}
             onToggle={handleToggle}
-            header={Crops.header}
+            header={inputData.crop || Crops.header}
             elements={Crops.elements}
+            icon={Crops.icon}
+            toggleHeader={Crops.toggleHeader}
           />
         </ComponentsContainer>
         <ComponentsContainer>
